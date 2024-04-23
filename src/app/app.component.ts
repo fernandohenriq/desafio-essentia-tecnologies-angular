8f5ae4f3ef98c885cdd592c2a80e268b4c8ca8c7
Fernando Henrique <fernandohenriqsilva@gmail.com>
2024-04-23 08:44:19 -0300

    refactor: change app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
     <div class="h-svh w-svw flex flex-col  bg-blue-800 text-slate-700">
       <header class="flex justify-center px-4 py-6 text-white">
         <a [routerLink]="['/']" class="text-decoration-none">
-          <span class="h1 text-7xl font-bold">Todo App</span>
+          <span class="h1 text-7xl font-bold">TechX</span>
         </a>
       </header>
       <section class="h-full w-full pb-10 flex flex-col justify-start items-center">
