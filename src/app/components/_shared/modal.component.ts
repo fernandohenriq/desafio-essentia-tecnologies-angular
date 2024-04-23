c3bcd2c1b56551537e8736ecb2710992300d335f
Fernando Henrique <fernandohenriqsilva@gmail.com>
2024-04-23 08:43:52 -0300

    refactor: move templetes to html files

--- src/app/components/_shared/modal.component.ts
+++ src/app/components/_shared/modal.component.ts
@@ -5,34 +5,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   selector: 'app-modal',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div
-      *ngIf="show"
-      (click)="onShadowClick()"
-      class="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center"
-    >
-      <div class="bg-white p-8 rounded-lg" (click)="$event.stopPropagation()">
-        <ng-content></ng-content>
-        <div class="flex justify-end mt-4">
-          <button
-            (click)="onConfirm()"
-            class="bg-blue-500 text-white px-4 py-2 rounded mr-2"
-            *ngIf="confirm.observers.length > 0"
-          >
-            Confirm
-          </button>
-          <button
-            (click)="onCancel()"
-            class="bg-gray-300 text-gray-700 px-4 py-2 rounded"
-            *ngIf="cancel.observers.length > 0"
-          >
-            Cancelar
-          </button>
-        </div>
-      </div>
-    </div>
-  `,
-  styles: ``,
+  templateUrl: './modal.component.html',
 })
 export class ModalComponent {
   @Input() show: boolean = false;
